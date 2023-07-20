f77945dc2af2b2c94fd9ecb7a5b0c27bbaedcf71
Bhavesh <mahajanbhaveshop2@gmail.com>
2023-07-20 11:45:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home/Home";
+// import EventPage from "./pages/EventPage/EventPage";
+// import Events from "../src/components/Events/Events";
 
-function App() {
+import { Route, Routes } from "react-router-dom";
+import { EventPage } from "./pages/EventPage/EventPage";
+import Events from "./components/Events/Events";
+import Speaker from "./components/Speaker/Speaker";
+import About from "./components/About/About";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+function App({Components,pageProps}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <NavBar/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/eventPage" element={<EventPage />} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/events" element={<Events/>} />
+        <Route path="/speakers" element={<Speaker/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      {/* <Components {...pageProps} /> */}
+      {/* <Footer/> */}
     </div>
   );
 }
